d1875e225c88d8ef38e41c748a647ee02770f226
zhoubo <zhoubo@ienc.cn>
2019-02-19 10:02:31 +0800

    2-19

---
+++ uaa/src/main/java/bo/zhou/uaa/utils/CodeGenerator.java
@@ -0,0 +1,144 @@
+package bo.zhou.uaa.utils;
+
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.builder.ConfigBuilder;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import org.apache.commons.lang.StringUtils;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+@Component
+@Scope("prototype")
+public class CodeGenerator extends AutoGenerator{
+
+    @PostConstruct
+    public void init(){
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://localhost:3306/zuul-auth?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=Hongkong");
+        // dsc.setSchemaName("public");
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("123456");
+        this.setDataSource(dsc);
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        this.setStrategy(strategy);
+    }
+
+    public List<TableInfo> getTableInfo(){
+        // 初始化配置
+        if (null == config) {
+            config = new ConfigBuilder(this.getPackageInfo(), this.getDataSource(), this.getStrategy(), this.getTemplate(), this.getGlobalConfig());
+            if (null != injectionConfig) {
+                injectionConfig.setConfig(config);
+            }
+        }
+        this.pretreatmentConfigBuilder(config);
+       return this.getAllTableInfoList(this.config);
+    }
+    /**
+     * <p>
+     * 读取控制台内容
+     * </p>
+     */
+    public static String scanner(String tip) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder help = new StringBuilder();
+        help.append("请输入" + tip + "：");
+        System.out.println(help.toString());
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StringUtils.isNotEmpty(ipt)) {
+                return ipt;
+            }
+        }
+        throw new MybatisPlusException("请输入正确的" + tip + "！");
+    }
+
+    public static void main(String[] args) {
+        // 代码生成器
+        CodeGenerator mpg = new CodeGenerator();
+
+        // 全局配置
+//        GlobalConfig gc = new GlobalConfig();
+//        String projectPath = System.getProperty("user.dir");
+//        gc.setOutputDir(projectPath +"//uaa"+ "/src/main/java");
+//        gc.setAuthor("zhoubo");
+//        gc.setOpen(false); gc.setFileOverride(true);
+//        gc.setActiveRecord(false);// 不需要ActiveRecord特性的请改为false
+//        gc.setEnableCache(false);// XML 二级缓存
+//        gc.setBaseResultMap(true);// XML ResultMap
+//        gc.setBaseColumnList(true);// XML columList
+//        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://localhost:3306/zuul-auth?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=Hongkong");
+        // dsc.setSchemaName("public");
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("123456");
+        mpg.setDataSource(dsc);
+
+        // 包配置
+//        PackageConfig pc = new PackageConfig();
+//        pc.setModuleName(scanner("模块名"));
+//        pc.setParent("bo.zhou");
+//        mpg.setPackageInfo(pc);
+
+//        // 自定义配置
+//        InjectionConfig cfg = new InjectionConfig() {
+//            @Override
+//            public void initMap() {
+//                // to do nothing
+//            }
+//        };
+//        List<FileOutConfig> focList = new ArrayList<>();
+//        focList.add(new FileOutConfig("/templates/mapper.xml.ftl") {
+//            @Override
+//            public String outputFile(TableInfo tableInfo) {
+//                // 自定义输入文件名称
+//                return projectPath +"//uaa" + "/src/main/resources/mapper/" + pc.getModuleName()
+//                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+//            }
+//        });
+//        cfg.setFileOutConfigList(focList);
+//        mpg.setCfg(cfg);
+//        mpg.setTemplate(new TemplateConfig().setXml(null));
+//
+//        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+////        strategy.setSuperEntityClass("com.baomidou.ant.common.BaseEntity");
+//        strategy.setEntityLombokModel(true);
+//        strategy.setRestControllerStyle(true);
+////        strategy.setSuperControllerClass("com.baomidou.ant.common.BaseController");
+//        strategy.setInclude(scanner("表名"));
+////        strategy.setSuperEntityColumns("id");
+//        strategy.setControllerMappingHyphenStyle(true);
+//        strategy.setTablePrefix(pc.getModuleName() + "_");
+        mpg.setStrategy(strategy);
+//        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        List<TableInfo> tableInfoList = mpg.getTableInfo();
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            System.out.println(objectMapper.writeValueAsString(tableInfoList));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        mpg.execute();
+    }
+
+}
