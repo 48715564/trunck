c306444d4ad165c9b0ddc21bb8d2b07e041ac6fc
zhoubo <zhoubo@ienc.cn>
2019-07-19 10:30:47 +0800

    7-19

--- service/src/main/java/bo/zhou/service/client/UserApi.java
+++ service/src/main/java/bo/zhou/service/client/UserApi.java
@@ -1,8 +1,6 @@
 package bo.zhou.service.client;
 
 import bo.zhou.common.client.AuthorizedFeignClient;
-import bo.zhou.common.client.AuthorizedUserFeignClient;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -11,7 +9,7 @@ import java.security.Principal;
 @RequestMapping("/api")
 public interface UserApi {
     @RequestMapping("/user")
-    public Principal user(Principal user);
+    Principal user(Principal user);
     @RequestMapping("/test")
-    public String test();
+    String test();
 }
